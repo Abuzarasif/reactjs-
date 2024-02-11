0749041f295b8e0842bc3e5594e1a7a901e536c9
Abuzar Asif <107269011+Abuzarasif@users.noreply.github.com>
2024-02-11 15:24:08 +0500

    Add files via upload

---
+++ Navigate.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import {useNavigate} from 'react-router-dom'
+const Navigate = () => {
+  return (
+    <div>
+      <h1> navigate</h1>
+      <button>Got</button>
+    </div>
+  )
+}
+
+export default Navigate
